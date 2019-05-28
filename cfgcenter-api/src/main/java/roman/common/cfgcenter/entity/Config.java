0ec056c3a634641255586da609254622eb42802d
roman.luo <roman.luo@vipshop.com>
2019-05-28 17:39:45 +0800

    init

--- cfgcenter-api/src/main/java/roman/common/cfgcenter/entity/Config.java
+++ cfgcenter-api/src/main/java/roman/common/cfgcenter/entity/Config.java
@@ -18,44 +18,50 @@ import java.util.Date;
 public class Config implements Identity {
 
     /**
-    *
+    * 自增ID
     */
     @Id
     @GeneratedValue(generator = "JDBC")
     private Long id;
 
     /**
-    *
+    * 配置域
     */
     @Column(name = "`domain`")
     private String domain;
 
     /**
-    *
+    * 配置key
     */
     @Column(name = "`key`")
     private String key;
 
     /**
-    *
+    * 配置value
     */
     @Column(name = "`value`")
     private String value;
 
     /**
-    *
+     * 配置说明
+     */
+    @Column(name = "`desc`")
+    private String desc;
+
+    /**
+    * 是否删除
     */
     @Column(name = "`is_deleted`")
     private Boolean isDeleted;
 
     /**
-    *
+    * 更新时间
     */
     @Column(name = "`update_time`")
     private Date updateTime;
 
     /**
-     *
+     * 创建时间
      */
     @Column(name = "`create_time`")
     private Date createTime;
--- config.sql
+++ config.sql
@@ -3,6 +3,7 @@ CREATE TABLE `config` (
   `domain` varchar(128) NOT NULL COMMENT '配置域',
   `key` varchar(64) NOT NULL COMMENT '配置key',
   `value` varchar(255) NOT NULL COMMENT '配置value',
+  `desc` varchar(255) NOT NULL COMMENT '配置说明',
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
